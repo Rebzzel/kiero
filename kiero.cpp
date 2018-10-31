0213928582c29a9b62239d21adad4d7db9de3c03
Rebzzel <rebzzel@gmail.com>
2018-10-31 17:34:44 +0300

    First commit

---
+++ kiero.cpp
@@ -0,0 +1,439 @@
+#include "kiero.h"
+#include <Windows.h>
+
+// Uncomment a needed graphical library (you can include all)
+//#include <d3d9.h>         // D3D9
+//#include <dxgi.h>         // D3D10/D3D11
+//#include <d3d10_1.h>      // D3D10
+//#include <d3d10.h>        // D3D10
+//#define KIERO_D3D10_USAGE // This need because d3d11.h includes d3d10.h
+//#include <d3d11.h>        // D3D11
+//#include <gl/GL.h>        // OpenGL
+
+#if defined(KIERO_D3D10_USAGE) && !defined(__d3d10_h__)
+#error KIERO_D3D10_USAGE defined, but d3d10.h not included
+#endif
+
+static kiero::RenderType::Enum g_renderType = kiero::RenderType::None;
+
+#if KIERO_ARCH_X64
+static uint64_t* g_methodsTable = NULL;
+#else
+static uint32_t* g_methodsTable = NULL;
+#endif
+
+kiero::Status::Enum kiero::init(RenderType::Enum _renderType)
+{
+	if (_renderType != RenderType::None)
+	{
+		if (_renderType >= RenderType::D3D9 && _renderType <= RenderType::D3D12) 
+		{
+			WNDCLASSEX windowClass;
+			windowClass.cbSize = sizeof(WNDCLASSEX);
+			windowClass.style = CS_HREDRAW | CS_VREDRAW;
+			windowClass.lpfnWndProc = DefWindowProc;
+			windowClass.cbClsExtra = 0;
+			windowClass.cbWndExtra = 0;
+			windowClass.hInstance = GetModuleHandle(NULL);
+			windowClass.hIcon = NULL;
+			windowClass.hCursor = NULL;
+			windowClass.hbrBackground = NULL;
+			windowClass.lpszMenuName = NULL;
+			windowClass.lpszClassName = KIERO_TEXT("Kiero");
+			windowClass.hIconSm = NULL;
+
+			::RegisterClassEx(&windowClass);
+
+			HWND window = ::CreateWindow(windowClass.lpszClassName, KIERO_TEXT("Kiero DirectX Window"), WS_OVERLAPPEDWINDOW, 0, 0, 100, 100, NULL, NULL, windowClass.hInstance, NULL);
+
+			if (_renderType == RenderType::D3D9)
+			{
+#ifdef _D3D9_H_
+				HMODULE libD3D9;
+				if ((libD3D9 = ::GetModuleHandle(KIERO_TEXT("d3d9.dll"))) == NULL)
+				{
+					::DestroyWindow(window);
+					::UnregisterClass(windowClass.lpszClassName, windowClass.hInstance);
+					return Status::ModuleNotFoundError;
+				}
+
+				void* Direct3DCreate9;
+				if ((Direct3DCreate9 = ::GetProcAddress(libD3D9, "Direct3DCreate9")) == NULL)
+				{
+					::DestroyWindow(window);
+					::UnregisterClass(windowClass.lpszClassName, windowClass.hInstance);
+					return Status::UnknownError;
+				}
+
+				LPDIRECT3D9 direct3D9;
+				if ((direct3D9 = ((LPDIRECT3D9(__stdcall*)(uint32_t))(Direct3DCreate9))(D3D_SDK_VERSION)) == NULL)
+				{
+					::DestroyWindow(window);
+					::UnregisterClass(windowClass.lpszClassName, windowClass.hInstance);
+					return Status::UnknownError;
+				}
+
+				D3DDISPLAYMODE displayMode;
+				if (direct3D9->GetAdapterDisplayMode(D3DADAPTER_DEFAULT, &displayMode) < 0)
+				{
+					::DestroyWindow(window);
+					::UnregisterClass(windowClass.lpszClassName, windowClass.hInstance);
+					return Status::UnknownError;
+				}
+
+				D3DPRESENT_PARAMETERS params;
+				params.BackBufferWidth = 0;
+				params.BackBufferHeight = 0;
+				params.BackBufferFormat = displayMode.Format;
+				params.BackBufferCount = 0;
+				params.MultiSampleType = D3DMULTISAMPLE_NONE;
+				params.MultiSampleQuality = NULL;
+				params.SwapEffect = D3DSWAPEFFECT_DISCARD;
+				params.hDeviceWindow = window;
+				params.Windowed = 1;
+				params.EnableAutoDepthStencil = 0;
+				params.AutoDepthStencilFormat = D3DFMT_UNKNOWN;
+				params.Flags = NULL;
+				params.FullScreen_RefreshRateInHz = 0;
+				params.PresentationInterval = 0;
+
+				LPDIRECT3DDEVICE9 device;
+				if (direct3D9->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, window, D3DCREATE_SOFTWARE_VERTEXPROCESSING | D3DCREATE_DISABLE_DRIVER_MANAGEMENT, &params, &device) < 0)
+				{
+					direct3D9->Release();
+					::DestroyWindow(window);
+					::UnregisterClass(windowClass.lpszClassName, windowClass.hInstance);	
+					return Status::UnknownError;
+				}
+
+#if KIERO_ARCH_X64
+				g_methodsTable = (uint64_t*)::malloc(119);
+				::memcpy(g_methodsTable, *(uint64_t**)device, 119 * sizeof(uint64_t));
+#else
+				g_methodsTable = (uint32_t*)::malloc(119);
+				::memcpy(g_methodsTable, *(uint32_t**)device, 119 * sizeof(uint32_t));
+#endif
+
+				direct3D9->Release();
+				direct3D9 = NULL;
+
+				device->Release();
+				device = NULL;
+
+				g_renderType = RenderType::D3D9;
+
+				::DestroyWindow(window);
+				::UnregisterClass(windowClass.lpszClassName, windowClass.hInstance);
+
+				return Status::Success;
+#endif
+			}
+			else if (_renderType == RenderType::D3D10)
+			{
+#if defined(__d3d10_h__) && defined(KIERO_D3D10_USAGE)
+				HMODULE libDXGI;
+				HMODULE libD3D10;
+				if ((libDXGI = ::GetModuleHandle(KIERO_TEXT("dxgi.dll"))) == NULL || (libD3D10 = ::GetModuleHandle(KIERO_TEXT("d3d10.dll"))) == NULL)
+				{
+					::DestroyWindow(window);
+					::UnregisterClass(windowClass.lpszClassName, windowClass.hInstance);
+					return Status::ModuleNotFoundError;
+				}
+
+				void* CreateDXGIFactory;
+				if ((CreateDXGIFactory = ::GetProcAddress(libDXGI, "CreateDXGIFactory")) == NULL)
+				{
+					::DestroyWindow(window);
+					::UnregisterClass(windowClass.lpszClassName, windowClass.hInstance);
+					return Status::UnknownError;
+				}
+
+				IDXGIFactory* factory;
+				if (((long(__stdcall*)(const IID&, void**))(CreateDXGIFactory))(__uuidof(IDXGIFactory), (void**)&factory) < 0)
+				{
+					::DestroyWindow(window);
+					::UnregisterClass(windowClass.lpszClassName, windowClass.hInstance);
+					return Status::UnknownError;
+				}
+
+				IDXGIAdapter* adapter;
+				if (factory->EnumAdapters(0, &adapter) == DXGI_ERROR_NOT_FOUND)
+				{
+					::DestroyWindow(window);
+					::UnregisterClass(windowClass.lpszClassName, windowClass.hInstance);
+					return Status::UnknownError;
+				}
+
+				void* D3D10CreateDeviceAndSwapChain;
+				if ((D3D10CreateDeviceAndSwapChain = ::GetProcAddress(libD3D10, "D3D10CreateDeviceAndSwapChain")) == NULL)
+				{
+					::DestroyWindow(window);
+					::UnregisterClass(windowClass.lpszClassName, windowClass.hInstance);
+					return Status::UnknownError;
+				}
+
+				DXGI_RATIONAL refreshRate;
+				refreshRate.Numerator = 60;
+				refreshRate.Denominator = 1;
+
+				DXGI_MODE_DESC bufferDesc;
+				bufferDesc.Width = 100;
+				bufferDesc.Height = 100;
+				bufferDesc.RefreshRate = refreshRate;
+				bufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+				bufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+				bufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+
+				DXGI_SAMPLE_DESC sampleDesc;
+				sampleDesc.Count = 1;
+				sampleDesc.Quality = 0;
+
+				DXGI_SWAP_CHAIN_DESC swapChainDesc;
+				swapChainDesc.BufferDesc = bufferDesc;
+				swapChainDesc.SampleDesc = sampleDesc;
+				swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+				swapChainDesc.BufferCount = 1;
+				swapChainDesc.OutputWindow = window;
+				swapChainDesc.Windowed = 1;
+				swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+				swapChainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+				IDXGISwapChain* swapChain;
+				ID3D10Device* device;
+
+				if (((long(__stdcall*)(
+					IDXGIAdapter*,
+					D3D10_DRIVER_TYPE,
+					HMODULE,
+					UINT,
+					UINT,
+					DXGI_SWAP_CHAIN_DESC*,
+					IDXGISwapChain**,
+					ID3D10Device**))(D3D10CreateDeviceAndSwapChain))(adapter, D3D10_DRIVER_TYPE_HARDWARE, NULL, 0, D3D10_SDK_VERSION, &swapChainDesc, &swapChain, &device) < 0)
+				{
+					::DestroyWindow(window);
+					::UnregisterClass(windowClass.lpszClassName, windowClass.hInstance);
+					return Status::UnknownError;
+				}
+
+#if KIERO_ARCH_X64
+				g_methodsTable = (uint64_t*)::malloc(18);
+				::memcpy(g_methodsTable, *(uint64_t**)swapChain, 18 * sizeof(uint64_t));
+#else
+				g_methodsTable = (uint32_t*)::malloc(18);
+				::memcpy(g_methodsTable, *(uint32_t**)swapChain, 18 * sizeof(uint32_t));
+#endif
+
+				swapChain->Release();
+				swapChain = NULL;
+
+				device->Release();
+				device = NULL;
+
+				g_renderType = RenderType::D3D10;
+
+				return Status::Success;
+#endif
+			}
+			else if (_renderType == RenderType::D3D11)
+			{
+#ifdef __d3d11_h__
+				HMODULE libD3D11;
+				if ((libD3D11 = ::GetModuleHandle(KIERO_TEXT("d3d11.dll"))) == NULL)
+				{
+					::DestroyWindow(window);
+					::UnregisterClass(windowClass.lpszClassName, windowClass.hInstance);
+					return Status::ModuleNotFoundError;
+				}
+
+				void* D3D11CreateDeviceAndSwapChain;
+				if ((D3D11CreateDeviceAndSwapChain = ::GetProcAddress(libD3D11, "D3D11CreateDeviceAndSwapChain")) == NULL)
+				{
+					::DestroyWindow(window);
+					::UnregisterClass(windowClass.lpszClassName, windowClass.hInstance);
+					return Status::UnknownError;
+				}
+
+				D3D_FEATURE_LEVEL featureLevel;
+				const D3D_FEATURE_LEVEL featureLevels[] = { D3D_FEATURE_LEVEL_10_0, D3D_FEATURE_LEVEL_10_1, D3D_FEATURE_LEVEL_11_0 };
+
+				DXGI_RATIONAL refreshRate;
+				refreshRate.Numerator = 60;
+				refreshRate.Denominator = 1;
+
+				DXGI_MODE_DESC bufferDesc;
+				bufferDesc.Width = 100;
+				bufferDesc.Height = 100;
+				bufferDesc.RefreshRate = refreshRate;
+				bufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+				bufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+				bufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+
+				DXGI_SAMPLE_DESC sampleDesc;
+				sampleDesc.Count = 1;
+				sampleDesc.Quality = 0;
+
+				DXGI_SWAP_CHAIN_DESC swapChainDesc;
+				swapChainDesc.BufferDesc = bufferDesc;
+				swapChainDesc.SampleDesc = sampleDesc;
+				swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+				swapChainDesc.BufferCount = 1;
+				swapChainDesc.OutputWindow = window;
+				swapChainDesc.Windowed = 1;
+				swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+				swapChainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+				IDXGISwapChain* swapChain;
+				ID3D11Device* device;
+				ID3D11DeviceContext* context;
+
+				if (((long(__stdcall*)(
+					IDXGIAdapter*,
+					D3D_DRIVER_TYPE,
+					HMODULE,
+					UINT,
+					const D3D_FEATURE_LEVEL*,
+					UINT,
+					UINT,
+					const DXGI_SWAP_CHAIN_DESC*,
+					IDXGISwapChain**,
+					ID3D11Device**,
+					D3D_FEATURE_LEVEL*,
+					ID3D11DeviceContext**))(D3D11CreateDeviceAndSwapChain))(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, 0, featureLevels, 1, D3D11_SDK_VERSION, &swapChainDesc, &swapChain, &device, &featureLevel, &context) < 0)
+				{
+					::DestroyWindow(window);
+					::UnregisterClass(windowClass.lpszClassName, windowClass.hInstance);
+					return Status::UnknownError;
+				}
+
+#if KIERO_ARCH_X64
+				g_methodsTable = (uint64_t*)::malloc(18);
+				::memcpy(g_methodsTable, *(uint64_t**)swapChain, 18 * sizeof(uint64_t));
+#else
+				g_methodsTable = (uint32_t*)::malloc(18);
+				::memcpy(g_methodsTable, *(uint32_t**)swapChain, 18 * sizeof(uint32_t));
+#endif
+
+				swapChain->Release();
+				swapChain = NULL;
+
+				device->Release();
+				device = NULL;
+
+				context->Release();
+				context = NULL;
+
+				g_renderType = RenderType::D3D11;
+
+				return Status::Success;
+#endif
+			}
+
+			// TODO: Need write hook for D3D12
+
+			return Status::NotSupportedError;
+		}
+		else if (_renderType == RenderType::OpenGL)
+		{
+#ifdef __gl_h_
+			HMODULE libOpenGL32;
+			if ((libOpenGL32 = ::GetModuleHandle(KIERO_TEXT("opengl32.dll"))) == NULL)
+			{
+				return Status::ModuleNotFoundError;
+			}
+
+			const char* const methodsNames[] = { 
+				"glAccum", "glAlphaFunc", "glAreTexturesResident", "glArrayElement", "glBegin", "glBindTexture", "glBitmap", "glBlendFunc", "glCallList", "glCallLists", "glClear", "glClearAccum", 
+				"glClearColor", "glClearDepth", "glClearIndex", "glClearStencil", "glClipPlane", "glColor3b", "glColor3bv", "glColor3d", "glColor3dv", "glColor3f", "glColor3fv", "glColor3i", "glColor3iv",
+				"glColor3s", "glColor3sv", "glColor3ub", "glColor3ubv", "glColor3ui", "glColor3uiv", "glColor3us", "glColor3usv", "glColor4b", "glColor4bv", "glColor4d", "glColor4dv", "glColor4f", 
+				"glColor4fv", "glColor4i", "glColor4iv", "glColor4s", "glColor4sv", "glColor4ub", "glColor4ubv", "glColor4ui", "glColor4uiv", "glColor4us", "glColor4usv", "glColorMask", "glColorMaterial",
+				"glColorPointer", "glCopyPixels", "glCopyTexImage1D", "glCopyTexImage2D", "glCopyTexSubImage1D", "glCopyTexSubImage2D", "glCullFaceglCullFace", "glDeleteLists", "glDeleteTextures",
+				"glDepthFunc", "glDepthMask", "glDepthRange", "glDisable", "glDisableClientState", "glDrawArrays", "glDrawBuffer", "glDrawElements", "glDrawPixels", "glEdgeFlag", "glEdgeFlagPointer",
+				"glEdgeFlagv", "glEnable", "glEnableClientState", "glEnd", "glEndList", "glEvalCoord1d", "glEvalCoord1dv", "glEvalCoord1f", "glEvalCoord1fv", "glEvalCoord2d", "glEvalCoord2dv",
+				"glEvalCoord2f", "glEvalCoord2fv", "glEvalMesh1", "glEvalMesh2", "glEvalPoint1", "glEvalPoint2", "glFeedbackBuffer", "glFinish", "glFlush", "glFogf", "glFogfv", "glFogi", "glFogiv",
+				"glFrontFace", "glFrustum", "glGenLists", "glGenTextures", "glGetBooleanv", "glGetClipPlane", "glGetDoublev", "glGetError", "glGetFloatv", "glGetIntegerv", "glGetLightfv", "glGetLightiv",
+				"glGetMapdv", "glGetMapfv", "glGetMapiv", "glGetMaterialfv", "glGetMaterialiv", "glGetPixelMapfv", "glGetPixelMapuiv", "glGetPixelMapusv", "glGetPointerv", "glGetPolygonStipple",
+				"glGetString", "glGetTexEnvfv", "glGetTexEnviv", "glGetTexGendv", "glGetTexGenfv", "glGetTexGeniv", "glGetTexImage", "glGetTexLevelParameterfv", "glGetTexLevelParameteriv", 
+				"glGetTexParameterfv", "glGetTexParameteriv", "glHint", "glIndexMask", "glIndexPointer", "glIndexd", "glIndexdv", "glIndexf", "glIndexfv", "glIndexi", "glIndexiv", "glIndexs", "glIndexsv",
+				"glIndexub", "glIndexubv", "glInitNames", "glInterleavedArrays", "glIsEnabled", "glIsList", "glIsTexture", "glLightModelf", "glLightModelfv", "glLightModeli", "glLightModeliv", "glLightf",
+				"glLightfv", "glLighti", "glLightiv", "glLineStipple", "glLineWidth", "glListBase", "glLoadIdentity", "glLoadMatrixd", "glLoadMatrixf", "glLoadName", "glLogicOp", "glMap1d", "glMap1f",
+				"glMap2d", "glMap2f", "glMapGrid1d", "glMapGrid1f", "glMapGrid2d", "glMapGrid2f", "glMaterialf", "glMaterialfv", "glMateriali", "glMaterialiv", "glMatrixMode", "glMultMatrixd", 
+				"glMultMatrixf", "glNewList", "glNormal3b", "glNormal3bv", "glNormal3d", "glNormal3dv", "glNormal3f", "glNormal3fv", "glNormal3i", "glNormal3iv", "glNormal3s", "glNormal3sv", 
+				"glNormalPointer", "glOrtho", "glPassThrough", "glPixelMapfv", "glPixelMapuiv", "glPixelMapusv", "glPixelStoref", "glPixelStorei", "glPixelTransferf", "glPixelTransferi", "glPixelZoom",
+				"glPointSize", "glPolygonMode", "glPolygonOffset", "glPolygonStipple", "glPopAttrib", "glPopClientAttrib", "glPopMatrix", "glPopName", "glPrioritizeTextures", "glPushAttrib", 
+				"glPushClientAttrib", "glPushMatrix", "glPushName", "glRasterPos2d", "glRasterPos2dv", "glRasterPos2f", "glRasterPos2fv", "glRasterPos2i", "glRasterPos2iv", "glRasterPos2s", 
+				"glRasterPos2sv", "glRasterPos3d", "glRasterPos3dv", "glRasterPos3f", "glRasterPos3fv", "glRasterPos3i", "glRasterPos3iv", "glRasterPos3s", "glRasterPos3sv", "glRasterPos4d", 
+				"glRasterPos4dv", "glRasterPos4f", "glRasterPos4fv", "glRasterPos4i", "glRasterPos4iv", "glRasterPos4s", "glRasterPos4sv", "glReadBuffer", "glReadPixels", "glRectd", "glRectdv", "glRectf",
+				"glRectfv", "glRecti", "glRectiv", "glRects", "glRectsv", "glRenderMode", "glRotated", "glRotatef", "glScaled", "glScalef", "glScissor", "glSelectBuffer", "glShadeModel", "glStencilFunc",
+				"glStencilMask", "glStencilOp", "glTexCoord1d", "glTexCoord1dv", "glTexCoord1f", "glTexCoord1fv", "glTexCoord1i", "glTexCoord1iv", "glTexCoord1s", "glTexCoord1sv", "glTexCoord2d", 
+				"glTexCoord2dv", "glTexCoord2f", "glTexCoord2fv", "glTexCoord2i", "glTexCoord2iv", "glTexCoord2s", "glTexCoord2sv", "glTexCoord3d", "glTexCoord3dv", "glTexCoord3f", "glTexCoord3fv", 
+				"glTexCoord3i", "glTexCoord3iv", "glTexCoord3s", "glTexCoord3sv", "glTexCoord4d", "glTexCoord4dv", "glTexCoord4f", "glTexCoord4fv", "glTexCoord4i", "glTexCoord4iv", "glTexCoord4s", 
+				"glTexCoord4sv", "glTexCoordPointer", "glTexEnvf", "glTexEnvfv", "glTexEnvi", "glTexEnviv", "glTexGend", "glTexGendv", "glTexGenf", "glTexGenfv", "glTexGeni", "glTexGeniv", "glTexImage1D",
+				"glTexImage2D", "glTexParameterf", "glTexParameterfv", "glTexParameteri", "glTexParameteriv", "glTexSubImage1D", "glTexSubImage2D", "glTranslated", "glTranslatef", "glVertex2d", 
+				"glVertex2dv", "glVertex2f", "glVertex2fv", "glVertex2i", "glVertex2iv", "glVertex2s", "glVertex2sv", "glVertex3d", "glVertex3dv", "glVertex3f", "glVertex3fv", "glVertex3i", "glVertex3iv",
+				"glVertex3s", "glVertex3sv", "glVertex4d", "glVertex4dv", "glVertex4f", "glVertex4fv", "glVertex4i", "glVertex4iv", "glVertex4s", "glVertex4sv", "glVertexPointer", "glViewport"
+			};
+
+			size_t size = KIERO_ARRAY_SIZE(methodsNames);
+
+#if KIERO_ARCH_X64
+			g_methodsTable = (uint32_t*)::malloc(size);
+
+			for (int i = 0; i < size; i++)
+			{
+				g_methodsTable[i] = (uint64_t)::GetProcAddress(libOpenGL32, methodsNames[i]);
+			}
+#else
+			g_methodsTable = (uint32_t*)::malloc(size);
+
+			for (int i = 0; i < size; i++)
+			{
+				g_methodsTable[i] = (uint32_t)::GetProcAddress(libOpenGL32, methodsNames[i]);
+			}
+#endif
+
+			g_renderType = RenderType::OpenGL;
+
+			return Status::Success;
+#endif
+		}
+
+		// TODO: Need write hook for Vulcan
+
+		return Status::NotSupportedError;
+	}
+
+	return Status::Success;
+}
+
+void kiero::shutdown()
+{
+	if (g_renderType > 0)
+	{
+		::free(g_methodsTable);
+		g_methodsTable = NULL;
+		g_renderType = RenderType::None;
+	}
+}
+
+kiero::RenderType::Enum kiero::getRenderType()
+{
+	return g_renderType;
+}
+
+#if KIERO_ARCH_X64
+uint64_t* kiero::getMethodsTable()
+{
+	return g_methodsTable;
+}
+#else
+uint32_t* kiero::getMethodsTable()
+{
+	return g_methodsTable;
+}
+#endif
+
+void kiero::bind(uint16_t _index, void* _original, void* _function)
+{
+	// TODO: This function must be as detour for methods from g_methodsTable
+}
---
+++ kiero.h
@@ -0,0 +1,73 @@
+#ifndef __KIERO_H__
+#define __KIERO_H__
+
+#include <stdint.h>
+#include <string.h>
+
+#define KIERO_VERSION "1.0.8"
+
+#define KIERO_ARCH_X64 0
+#define KIERO_ARCH_X86 0
+
+#if defined(_M_X64)	
+# undef  KIERO_ARCH_X64
+# define KIERO_ARCH_X64 1
+#else
+# undef  KIERO_ARCH_X86
+# define KIERO_ARCH_X86 1
+#endif
+
+#ifdef _UNICODE
+# define KIERO_TEXT(text) L##text
+#else
+# define KIERO_TEXT(text) text
+#endif
+
+#define KIERO_ARRAY_SIZE(arr) ((size_t)(sizeof(arr)/sizeof(arr[0])))
+
+namespace kiero
+{
+	struct Status
+	{
+		enum Enum
+		{
+			UnknownError = -1,
+			NotSupportedError = -2,
+			ModuleNotFoundError = -3,
+
+			Success = 0,
+		};
+	};
+
+	struct RenderType
+	{
+		enum Enum
+		{
+			None,
+
+			D3D9,   // Implemented
+			D3D10,  // Implemented
+			D3D11,  // Implemented
+			D3D12,  // In development
+
+			OpenGL, // Implemented
+			Vulcan  // In development
+		};
+	};
+
+	Status::Enum init(RenderType::Enum renderType);
+
+	void shutdown();
+
+	RenderType::Enum getRenderType();
+
+#if KIERO_ARCH_X64
+	uint64_t* getMethodsTable();
+#else
+	uint32_t* getMethodsTable();
+#endif
+
+	void bind(uint16_t index, void* original, void* function);
+}
+
+#endif // __KIERO_H__
