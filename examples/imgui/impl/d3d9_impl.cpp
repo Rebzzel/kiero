65262b733eaf7635ef6d9cea20dbe2eb607a3de6
Rebzzel <Rebzzel@gmail.com>
2020-03-22 16:05:17 +0300

    [imgui example]: fix crashes when changing window state (resize, change video mode, etc.) (d3d9)

--- examples/imgui/impl/d3d9_impl.cpp
+++ examples/imgui/impl/d3d9_impl.cpp
@@ -10,8 +10,20 @@
 #include "../imgui/examples/imgui_impl_win32.h"
 #include "../imgui/examples/imgui_impl_dx9.h"
 
+typedef long(__stdcall* Reset)(LPDIRECT3DDEVICE9, D3DPRESENT_PARAMETERS*);
+static Reset oReset = NULL;
+
 typedef long(__stdcall* EndScene)(LPDIRECT3DDEVICE9);
 static EndScene oEndScene = NULL;
+
+long __stdcall hkReset(LPDIRECT3DDEVICE9 pDevice, D3DPRESENT_PARAMETERS* pPresentationParameters)
+{
+	ImGui_ImplDX9_InvalidateDeviceObjects();
+	long result = oReset(pDevice, pPresentationParameters);
+	ImGui_ImplDX9_CreateDeviceObjects();
+
+	return result;
+}
 
 long __stdcall hkEndScene(LPDIRECT3DDEVICE9 pDevice)
 {
@@ -44,6 +56,7 @@ long __stdcall hkEndScene(LPDIRECT3DDEVICE9 pDevice)
 
 void impl::d3d9::init()
 {
+	assert(kiero::bind(16, (void**)&oReset, hkReset) == kiero::Status::Success);
 	assert(kiero::bind(42, (void**)&oEndScene, hkEndScene) == kiero::Status::Success);
 }
 
